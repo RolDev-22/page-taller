f2225e67dd9ac798fb0a9ebb12c0b54b92ae4cbf
RolDev-22 <rolandomuagui1629@gmail.com>
2026-01-02 18:10:24 -0600

    Reacomodamiento y estructuracion de components para una mejor escalabilidad y entendimeinto del proyectop, ademas, se agrega el logo de la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { About } from "./pages/About";
 import { Home } from "./pages/home";
 import { Service } from "./pages/Service";
-import Footer from "./components/footer";
-import Header from "./components/header";
+import Footer from "./components/Footer/footer";
+import Header from "./components/Header/header";
 import "./style/App.css";
 
 function App() {
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,4 +1,4 @@
-import "./style/header.css";
+import "./header.css";
 
 export default function header() {
   return <header>items</header>;
