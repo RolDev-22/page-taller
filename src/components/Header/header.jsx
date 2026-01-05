e14a00767a28ff81dc0222be6332507bde6b98f4
RolDev-22 <rolandomuagui1629@gmail.com>
2026-01-05 17:42:18 -0600

    Finalizacion de la seccionn home, se agregan logos de redes y leyendas, ademas, se corrige resp[onsive del header en todos los dispositivos

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,16 +1,31 @@
 import "./header.css";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Header() {
   const windowWidth = window.innerWidth;
+  const refToHeader = useRef(null);
   const [activeMenu, setActiveMenu] = useState(false);
 
   const showMenu = () => {
-    windowWidth <= 768 ? setActiveMenu(!activeMenu) : setActiveMenu(false);
+    windowWidth <= 1023 ? setActiveMenu(!activeMenu) : setActiveMenu(false);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 15
+        ? refToHeader.current.classList.add("addFilter")
+        : refToHeader.current.classList.remove("addFilter");
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }),
+    [];
+
   return (
-    <header className={`${activeMenu ? "showMenu" : ""}`}>
+    <header ref={refToHeader} className={`${activeMenu ? "showMenu" : ""}`}>
       <figure className="figure-logo">
         <a href="#section-Home">
           <img loading="lazy" src="./pg_logo.png" alt="Logo Empresarial" />
@@ -30,7 +45,7 @@ export default function Header() {
           </li>
           <li>
             <a href="#section-About" onClick={showMenu}>
-              Sobre Nosotros
+              Nosotros
             </a>
           </li>
           <li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,53 @@ const Home = () => {
   return (
     <section id="section-Home">
       <svg width="0" height="0">
-        <clipPath id="clip" clipPathUnits="objectBoundingBox">
-          <path d="M0,0.1 L0.25,0.1 A0.05,0.05 0,0 0.30,0.05 L0.30,0 L1,0 L1,1 L0,1 Z" />
+        <clipPath id="tallerClip" clipPathUnits="objectBoundingBox">
+          <path
+            d="
+      M 0,0.22
+      L0.20,0.22
+      A .05 .05 0 0 0
+      0.25,0.17
+      L0.25,0
+      L1,0
+      L1,1
+      L0,1
+      L0,0.22
+      Z"
+          />
         </clipPath>
       </svg>
-
-      <div className="container container-home">Hola home</div>
+      <div className="container container-home">
+        <article className="container-information">
+          <h2>TALLER AUTOMOTRIZ MOPRIS</h2>
+          <h3>
+            «No siempre es posible ser el mejor, pero siempre es posible mejorar
+            tu propio rendimiento»
+          </h3>
+        </article>
+        <article className="container-schedule">
+          <h3>Horario</h3>
+          <h4>Lunes - Sabado</h4>
+          <h5>7:00 a.m. - 5:00 p.m.</h5>
+        </article>
+        <article className="container-social">
+          <figure>
+            <a href="#" title="Siguenos en Instagram">
+              <img src="/ic-insta.svg" alt="logo-instagram" />
+            </a>
+          </figure>
+          <figure>
+            <a href="#" title="Siguenos en Facebook">
+              <img src="/ic-face.svg" alt="logo-facebook" />
+            </a>
+          </figure>
+          <figure>
+            <a href="#" title="Siguenos en Whatsapp">
+              <img src="/ic-whats.svg" alt="logo-whatsapp" />
+            </a>
+          </figure>
+        </article>
+      </div>
     </section>
   );
 };
